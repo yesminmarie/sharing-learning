1936c349c86b78fadfdfc107b41d88cd6eb54011
Yesmin Lahoud <ymslahoud@gmail.com>
2023-01-12 14:10:13 -0300

    Gateway

---
+++ gateway/src/main/java/io/github/yesminmarie/gateway/GatewayApplication.java
@@ -0,0 +1,25 @@
+package io.github.yesminmarie.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator routes(RouteLocatorBuilder builder){
+		return builder
+				.routes()
+				.route(r -> r.path("/users/**").uri("lb://users"))
+				.build();
+	}
+}
